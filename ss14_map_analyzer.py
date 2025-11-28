b01eace2389640288d22e8a65a55843b979dd6de
Jen Pollock <jen@jenpollock.ca>
2025-11-27 23:01:29 -0700

    Get info for all entities, break csv printing for now

--- ss14_map_analyzer.py
+++ ss14_map_analyzer.py
@@ -5,10 +5,24 @@ import pathlib
 import sys
 import yaml
 
+# Entities for which to report the number per thousand tiles
+density_entities = ["StationMap"]
+
+# For each prefix, group entities with names beginning with that prefix together
+grouping_prefixes = ["SignDirectional"]
+
+# Groups for which to report the number per thousand tiles
+density_groups = ["SignDirectional"]
+
 yaml.add_multi_constructor(u'!type:', lambda loader, suffix, node: loader.construct_mapping(node))
 
 def analyze_map(mappath):
-    map_info = {'tile_count': 0, 'maps': 0, 'directional_signs': 0}
+    map_info = {
+      'tile_count': 0,
+      'entity_counts': {},
+      'entity_group_counts': dict.fromkeys(grouping_prefixes, 0),
+      'densities': {}
+    }
 
     with open(mappath, 'r') as mapfile:
         mapdata = yaml.load(mapfile, yaml.Loader)
@@ -30,16 +44,23 @@ def analyze_map(mappath):
                             map_info['tile_count'] += tile_count
                     elif comp['type'] == 'BecomesStation':
                         map_info['id'] = comp['id']
-        elif ent['proto'] == 'StationMap':
-            map_info['maps'] = len(ent['entities'])
-        elif ent['proto'].startswith('SignDirectional'):
-            map_info['directional_signs'] += len(ent['entities'])
+        else:
+          added = False
+          for prefix in grouping_prefixes:
+            if ent['proto'].startswith(prefix):
+              map_info['entity_group_counts'][prefix] += len(ent['entities'])
+              added = True
+              break
+          if not added:
+            map_info['entity_counts'][ent['proto']] = len(ent['entities'])
 
-    if 'maps' in map_info:
-        map_info['maps_density'] = 1000 * map_info['maps']/map_info['tile_count']
+    for proto in density_entities:
+      if proto in map_info['entity_counts']:
+        map_info['densities'][proto + ' density'] = 1000 * map_info['entity_counts'][proto] / map_info['tile_count']
 
-    if 'directional_signs' in map_info:
-        map_info['directional_sign_density'] = 1000 * map_info['directional_signs']/map_info['tile_count']
+    for proto in density_groups:
+      if proto in map_info['entity_group_counts']:
+        map_info['densities'][proto + ' density'] = 1000 * map_info['entity_group_counts'][proto] / map_info['tile_count']
 
     return map_info
 
@@ -56,6 +77,9 @@ if __name__ == '__main__':
     all_maps_data = []
     for map_path in map_path_list:
         all_maps_data.append(analyze_map(map_path))
+
+    print(all_maps_data)
+    exit()
 
     writer = csv.DictWriter(sys.stdout, ['id', 'tile_count', 'maps', 'maps_density', 'directional_signs', 'directional_sign_density'])
     writer.writeheader()
